cf1b92edacaa7ca7611c44b7df268a53dcca5426
Ömer Sinan Ağacan <omeragacan@gmail.com>
2014-11-07 13:53:38 +0200

    fresh start

---
+++ src/Rho/Metainfo.hs
@@ -0,0 +1,115 @@
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric, FlexibleInstances,
+             OverloadedStrings, RecordWildCards, TemplateHaskell #-}
+{-# OPTIONS_HADDOCK show-extensions #-}
+
+module Rho.Metainfo where
+
+import           Control.Applicative
+import           Control.Monad
+import           Data.BEncode              as BE
+import qualified Data.BEncode.BDict        as BE
+import qualified Data.ByteString           as B
+import           Data.Typeable
+import           GHC.Generics
+
+-- | Metainfo file as specified in
+-- https://wiki.theory.org/BitTorrentSpecification#Metainfo_File_Structure
+data Metainfo = Metainfo
+  { mAnnounce     :: B.ByteString
+  , mAnnounceList :: Maybe [[B.ByteString]]
+  , mCreationDate :: Maybe Int
+  , mComment      :: Maybe B.ByteString
+  , mCreatedBy    :: Maybe B.ByteString
+  , mEncoding     :: Maybe B.ByteString
+  , mInfo         :: Info
+  } deriving (Show, Eq, Typeable, Generic)
+
+data Info = Info
+  { iName        :: B.ByteString
+  , iPieceLength :: Int
+  , iPieces      :: [B.ByteString]
+  , iPrivate     :: Bool
+  , iFiles       :: [File]
+  } deriving (Show, Eq, Typeable, Generic)
+
+data File = File
+  { fLength :: Int
+  , fMd5Sum :: Maybe B.ByteString
+  , fPath   :: [B.ByteString]
+  } deriving (Show, Eq, Typeable, Generic)
+
+-- | Parse contents of a .torrent file.
+parseMetainfo :: B.ByteString -> Result Metainfo
+parseMetainfo = decode
+
+-- * BEncode instances
+
+instance BEncode Metainfo where
+  toBEncode Metainfo{..} = toDict $
+       "announce"      .=! mAnnounce
+    .: "announce-list" .=? mAnnounceList
+    .: "creation date" .=? mCreationDate
+    .: "comment"       .=? mComment
+    .: "created by"    .=? mCreatedBy
+    .: "encoding"      .=? mEncoding
+    .: "info"          .=! mInfo
+    .: endDict
+
+  fromBEncode = fromDict $
+    Metainfo <$>! "announce"
+             <*>? "announce-list"
+             <*>? "creation date"
+             <*>? "comment"
+             <*>? "created by"
+             <*>? "encoding"
+             <*>! "info"
+
+instance BEncode Info where
+  toBEncode i = toDict $
+       "files"        .=! iFiles i
+    .: "name"         .=! iName i
+    .: "piece length" .=! iPieceLength i
+    .: "pieces"       .=! B.concat (iPieces i)
+    .: "private"      .=? (if iPrivate i then Just True else Nothing)
+    .: endDict
+
+  fromBEncode bv@(BDict (BE.Cons key _ _))
+    | key == "length" = flip fromDict bv $ do
+        -- first key is "length", single file mode
+        flen <- field $ req "length"
+        md5sum <- optional $ field $ req "md5sum"
+        name <- field $ req "name"
+        pieceLength <- field $ req "piece length"
+        pieces <- splitPieces <$> (field $ req "pieces")
+        private <- readPrivate
+        return $ Info name pieceLength pieces private [File flen md5sum []]
+    | otherwise = flip fromDict bv $ do
+        -- multi file mode
+        files <- field $ req "files"
+        name <- field $ req "name"
+        pieceLength <- field $ req "piece length"
+        pieces <- splitPieces <$> (field $ req "pieces")
+        private <- readPrivate
+        return $ Info name pieceLength pieces private files
+    where
+      readPrivate :: Get Bool
+      readPrivate = maybe False id <$> (optional $ field $ req "private")
+
+      readPieces :: Get [B.ByteString]
+      readPieces = do
+        bs <- field $ req "pieces"
+        return $ splitPieces bs
+
+      splitPieces :: B.ByteString -> [B.ByteString]
+      splitPieces bs
+        | B.length bs < 20 = []
+        | otherwise = let (h, t) = B.splitAt 20 bs in h : splitPieces t
+
+instance BEncode File where
+  toBEncode File{..} = toDict $
+       "length" .=! fLength
+    .: "md5sum" .=? fMd5Sum
+    .: "path"   .=! fPath
+    .: endDict
+
+  fromBEncode = fromDict $ File <$>! "length" <*>? "md5sum" <*>! "path"
---
+++ test/Rho/MetainfoSpec.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE CPP, TupleSections #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Rho.MetainfoSpec where
+
+import           Rho.Metainfo
+
+import           Control.Applicative
+import           Control.Monad
+import           Data.BEncode              as BE
+import qualified Data.ByteString           as B
+import           System.Directory          (getDirectoryContents)
+import           System.FilePath           (takeExtension, (</>))
+import           Test.Hspec
+import           Test.Hspec.HUnit
+import           Test.Hspec.QuickCheck
+import           Test.HUnit
+import           Test.QuickCheck           hiding (Result)
+import           Test.QuickCheck.Instances ()
+
+#if MIN_VERSION_base(4,7,0)
+import           Data.Either               (isLeft, isRight)
+#else
+isLeft :: Either a b -> Bool
+isLeft Left{} = True
+isLeft _      = False
+
+isRight :: Either a b -> Bool
+isRight Right{} = True
+isRight _       = False
+#endif
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "parsing" $ do
+    fromHUnitTest $ TestLabel "should parse" shouldParse
+    fromHUnitTest $ TestLabel "should not parse" shouldNotParse
+
+  describe "parsing-printing" $ do
+    prop "forall d, fromBEncode . toBEncode $ d = Right d" $ \d ->
+      (BE.fromBEncode . BE.toBEncode) (d :: Metainfo) `shouldBe` (Right d)
+
+shouldParse :: Test
+shouldParse = TestCase $ do
+  tfs <- getTorrentFiles "tests/should_parse"
+  sequence_ $ flip map tfs $ \(f, c) ->
+    assertBool ("Can't parse torrent file: " ++ f) $ isRight (decode c :: Result Metainfo)
+
+shouldNotParse :: Test
+shouldNotParse = TestCase $ do
+  tfs <- getTorrentFiles "tests/should_not_parse"
+  sequence_ $ flip map tfs $ \(f, c) ->
+    assertBool ("Parsed a broken torrent file: " ++ f) $ isLeft (decode c :: Result Metainfo)
+
+getTorrentFiles :: FilePath -> IO [(FilePath, B.ByteString)]
+getTorrentFiles root =
+  mapM (\p -> (p,) `fmap` B.readFile p) . filter ((==) ".torrent" . takeExtension) . map (root </>)
+    =<< getDirectoryContents root
+
+
+-- * Arbitrary instances
+
+instance Arbitrary Metainfo where
+  arbitrary =
+    liftM5 Metainfo arbitrary arbitrary arbitrary arbitrary arbitrary
+      <*> arbitrary <*> arbitrary
+  shrink = recursivelyShrink
+
+instance Arbitrary Info where
+  arbitrary = liftM5 Info arbitrary arbitrary (listOf (B.pack <$> vector 20)) arbitrary arbitrary
+  shrink = recursivelyShrink
+
+instance Arbitrary File where
+  arbitrary = File <$> arbitrary <*> arbitrary <*> arbitrary
+  shrink = recursivelyShrink
