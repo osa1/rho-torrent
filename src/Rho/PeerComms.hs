3ec0027affb7f82da0c3e468f8e67c3a1f2e310c
Ömer Sinan Ağacan <omeragacan@gmail.com>
2014-11-17 08:46:33 +0200

    yet another refactoring of peer comms.

--- src/Rho/PeerComms.hs
+++ src/Rho/PeerComms.hs
@@ -44,39 +44,57 @@ data PeerConn = PeerConn
   } deriving (Show)
 
 data PeerCommHandler = PeerCommHandler
-  { pchPeers :: MVar (M.Map SockAddr PeerConn)
-  , pchSock  :: Socket
+  { pchPeers   :: MVar (M.Map SockAddr PeerConn)
+  , pchMsgChan :: Chan (SockAddr, Socket, B.ByteString)
+  -- , pchSock  :: Socket
     -- ^ socket used to listen incoming messages
   }
 
+-- \ Initialize listeners, data structures etc. for peer communications.
 initPeerCommsHandler :: IO PeerCommHandler
 initPeerCommsHandler = do
+    -- state of all connected peers
     peers <- newMVar M.empty
-    -- used to pass bytes from socket listener to message handler
-    msgChan <- newChan
-    sock <- spawnPeerMsgListener msgChan
-    async $ peerMsgHandler peers msgChan
-    return $ PeerCommHandler peers sock
-
-spawnPeerMsgListener :: Chan (SockAddr, Socket, B.ByteString) -> IO Socket
-spawnPeerMsgListener msgChan = do
+    -- sock for incoming connections
     sock <- socket AF_INET Stream defaultProtocol
     bind sock (SockAddrInet (fromIntegral (5433 :: Int)) 0) -- TODO: hard-coded port
-    async $ peerMsgListener sock
-    return sock
-  where
-    peerMsgListener sock = do
-      (peerSocket, peerAddr) <- accept sock
-      msg <- recv peerSocket 10000
-      writeChan msgChan (peerAddr, peerSocket, msg)
-      peerMsgListener sock
+    -- channel from from socket listeners to message handler
+    msgChan <- newChan
+    _ <- spawnPeerSockListener sock msgChan
+    async $ peerMsgHandler peers msgChan
+    return $ PeerCommHandler peers msgChan
 
-peerMsgHandler
-    :: MVar (M.Map SockAddr PeerConn)
-    -> Chan (SockAddr, Socket, B.ByteString)
-    -> IO ()
+-- \ Accept incoming connections and spawn a connected socket listener for
+-- evrey accepted connection.
+spawnPeerSockListener :: Socket -> Chan (SockAddr, Socket, B.ByteString) -> IO (Async ())
+spawnPeerSockListener sock msgChan = async loop
+  where
+    loop = do
+      (peerSocket, peerAddr) <- accept sock
+      spawnConnectedSockListener peerSocket peerAddr msgChan
+      loop
+
+-- \ Listen socket of a connected peer and send bytes to channel.
+spawnConnectedSockListener
+    :: Socket -> SockAddr -> Chan (SockAddr, Socket, B.ByteString) -> IO (Async ())
+spawnConnectedSockListener sock sockAddr msgChan = async loop
+  where
+    loop = do
+      msg <- recv sock 10000
+      -- empty message == closed connection
+      unless (B.null msg) $ do
+        writeChan msgChan (sockAddr, sock, msg)
+        loop
+
+-- \ Parse incoming bytes and update data structures.
+peerMsgHandler :: MVar (M.Map SockAddr PeerConn) -> Chan (SockAddr, Socket, B.ByteString) -> IO ()
 peerMsgHandler peers msgChan = do
     (peerAddr, peerSock, peerMsg) <- readChan msgChan
+    handlePeerMsg peers peerAddr peerSock peerMsg
+    peerMsgHandler peers msgChan
+
+handlePeerMsg :: MVar (M.Map SockAddr PeerConn) -> SockAddr -> Socket -> B.ByteString -> IO ()
+handlePeerMsg peers peerAddr peerSock peerMsg = do
     -- have we established a connection with the peer?
     peers' <- readMVar peers
     case M.lookup peerAddr peers' of
@@ -91,30 +109,37 @@ peerMsgHandler peers msgChan = do
         case parsePeerMsg peerMsg of
           Nothing -> putStrLn "Can't parse peer msg."
           Just msg -> putStrLn $ "Parsed a peer msg: " ++ show msg
-    peerMsgHandler peers msgChan
 
 handshake :: PeerCommHandler -> SockAddr -> B.ByteString -> B.ByteString -> IO ()
-handshake PeerCommHandler{pchPeers=peers} addr infoHash peerId = do
+handshake PeerCommHandler{pchPeers=peers, pchMsgChan=msgChan} addr infoHash peerId = do
     ret <- sendHandshake addr infoHash peerId
     case ret of
       Left err -> putStrLn $ "Can't establish connection: " ++ err
-      Right (sock, infoHash, peerId, _msg) -> do
-        -- TODO: handle msg
+      Right (sock, infoHash, peerId, msg) -> do
         modifyMVar_ peers $ \peers' ->
           case M.lookup addr peers' of
             Nothing -> do
-              -- first time handshaking with the peer
+              -- first time handshaking with the peer, spawn a socket
+              -- listener
+              spawnConnectedSockListener sock addr msgChan
               -- TODO: check info_hash
               putStrLn "Handshake successful"
               return $ M.insert addr (PeerConn True False True False peerId [infoHash] sock) peers'
             Just pc -> do
               -- probably learned about a new torrent
               putStrLn "Handshake successful"
+              -- we already knew about this peer, so there should be
+              -- a thread listening for messages from this socket. no need
+              -- to create a new one.
               return $ M.insert addr pc{pcOffers = infoHash : pcOffers pc} peers'
+        writeChan msgChan (addr, sock, msg)
 
+-- \ Send a handshake message to given target using a fresh socket. Return
+-- the connected socket in case of a success. (e.g. receiving answer to
+-- handshake)
 sendHandshake
     :: SockAddr -> B.ByteString -> B.ByteString
-    -> IO (Either String (Socket, B.ByteString, B.ByteString, Maybe PeerMsg))
+    -> IO (Either String (Socket, B.ByteString, B.ByteString, B.ByteString))
 sendHandshake addr infoHash peerId = flip catchIOError errHandler $ do
     sock <- socket AF_INET Stream defaultProtocol
     bind sock (SockAddrInet aNY_PORT 0)
@@ -143,14 +168,14 @@ sendHandshake addr infoHash peerId = flip catchIOError errHandler $ do
     errHandler err =
       return $ Left $ "Unhandled error: " ++ show err
 
-parseHandshake :: B.ByteString -> Either String (B.ByteString, B.ByteString, Maybe PeerMsg)
+parseHandshake :: B.ByteString -> Either String (B.ByteString, B.ByteString, B.ByteString)
 parseHandshake bs =
     case execParser bs handshakeParser of
       Just ((pstr, infoHash, peerId), rest) -> do
         assert ("Unknown pstr: " ++ BC.unpack pstr) (pstr == "BitTorrent protocol")
         assert ("info_hash length is wrong: " ++ show (B.length infoHash)) (B.length infoHash == 20)
         assert ("peer_id length is wrong: " ++ show (B.length peerId)) (B.length peerId == 20)
-        return (infoHash, peerId, parsePeerMsg rest)
+        return (infoHash, peerId, rest)
       Nothing -> Left "Can't parse handshake message."
   where
     assert :: String -> Bool -> Either String ()
