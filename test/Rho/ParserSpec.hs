072c904e8f197beb75e8e25b03045a77bc53eb97
Ömer Sinan Ağacan <omeragacan@gmail.com>
2014-12-12 11:41:28 +0200

    remove unused imports

--- test/Rho/ParserSpec.hs
+++ test/Rho/ParserSpec.hs
@@ -2,19 +2,11 @@
 
 module Rho.ParserSpec where
 
-import           Control.Applicative
-import           Data.Bits
-import qualified Data.ByteString.Char8     as BC
-import           Data.List                 (foldl')
-import qualified Data.Set                  as S
-import           Data.Word
+import qualified Data.ByteString.Char8 as BC
 
 import           Test.Hspec
 import           Test.Hspec.HUnit
-import           Test.Hspec.QuickCheck
 import           Test.HUnit
-import           Test.QuickCheck           hiding (Result)
-import           Test.QuickCheck.Instances ()
 
 import           Rho.Tracker
 
