21e3e6ce2aa71b24f02ef70dacf514e4bb8ccf21
Ömer Sinan Ağacan <omeragacan@gmail.com>
2014-12-13 20:07:18 +0200

    refactoring: use vector instead of array. move a util.

--- src/Rho/PieceMgr.hs
+++ src/Rho/PieceMgr.hs
@@ -3,25 +3,24 @@ module Rho.PieceMgr where
 import           Control.Applicative
 import           Control.Concurrent.MVar
 import           Control.Monad
-import qualified Data.Array.IO           as A
-import qualified Data.ByteString         as B
-import qualified Data.ByteString.Builder as BB
-import qualified Data.ByteString.Char8   as BC
-import qualified Data.ByteString.Lazy    as LB
-import           Data.Digest.SHA1        (Word160 (..), hash)
-import           Data.List               (foldl')
-import           Data.Monoid
+import qualified Data.ByteString             as B
+import qualified Data.ByteString.Char8       as BC
+import           Data.Digest.SHA1            (hash)
+import           Data.List                   (foldl')
+import qualified Data.Vector.Unboxed         as V
+import qualified Data.Vector.Unboxed.Mutable as MV
 import           Data.Word
-import           System.FilePath         ((</>))
+import           System.FilePath             ((</>))
 
 import           Rho.Metainfo
+import           Rho.Utils
 
 data PieceMgr = PieceMgr
   { pmPieceSize :: Word32
   , pmTotalSize :: Word64
   , pmPieces    :: Word32
-  , pmData      :: MVar ( A.IOUArray Word64 Word8
-                        , A.IOUArray Word64 Bool )
+  , pmData      :: MVar ( MV.IOVector Word8
+                        , MV.IOVector Bool )
   }
 
 -- | (piece index, offset in piece, length until next piece) triple.
@@ -29,8 +28,8 @@ type PieceData = (Word32, Word32, Word32)
 
 newPieceMgr :: Word64 -> Word32 -> IO PieceMgr
 newPieceMgr totalSize pieceLength = do
-    arr <- A.newArray (0, totalSize - 1) 0
-    bits <- A.newArray (0, totalSize - 1) False
+    arr <- MV.new (fromIntegral totalSize)
+    bits <- MV.replicate (fromIntegral totalSize) False
     var <- newMVar (arr, bits)
     let pieces =
           let (d, m) = totalSize `divMod` fromIntegral pieceLength
@@ -42,8 +41,8 @@ writePiece (PieceMgr ps _ _ m) pieceIdx pieceOffset pieceData = do
     (arr, bits) <- takeMVar m
     zipWithM_ (\byteIdx byte -> do
       let arrIdx = fromIntegral ps * fromIntegral pieceIdx + fromIntegral pieceOffset
-      A.writeArray arr  (arrIdx + byteIdx) byte
-      A.writeArray bits (arrIdx + byteIdx) True)
+      MV.write arr  (arrIdx + byteIdx) byte
+      MV.write bits (arrIdx + byteIdx) True)
       [0..] (B.unpack pieceData)
     putMVar m (arr, bits)
 
@@ -60,14 +59,14 @@ missingPieces (PieceMgr ps ts totalPieces m) = do
     return ret
   where
     collectMissingPieces
-      :: A.IOUArray Word64 Word8 -> A.IOUArray Word64 Bool -> Word64 -> Word64
+      :: MV.IOVector Word8 -> MV.IOVector Bool -> Word64 -> Word64
       -> IO [(Word32, Word32, Word32)]
-    collectMissingPieces arr bits max idx
-      | idx > max = return []
+    collectMissingPieces arr bits end idx
+      | idx > end = return []
       | otherwise = do
-          bit <- A.readArray bits idx
+          bit <- MV.read bits (fromIntegral idx)
           if bit
-            then collectMissingPieces arr bits max (idx + 1)
+            then collectMissingPieces arr bits end (idx + 1)
             else do
               let offsetToNextPiece, currentPiece, pieceOffset :: Word32
                   offsetToNextPiece = fromIntegral $ fromIntegral ps - (idx `mod` fromIntegral ps)
@@ -79,36 +78,43 @@ missingPieces (PieceMgr ps ts totalPieces m) = do
                   let md = ts `mod` fromIntegral ps
                   return [(currentPiece, 0, if md == 0 then ps else fromIntegral md)]
                 else do
-                  rest <- collectMissingPieces arr bits max (idx + fromIntegral offsetToNextPiece)
+                  rest <- collectMissingPieces arr bits end (idx + fromIntegral offsetToNextPiece)
                   return $ (currentPiece, pieceOffset, offsetToNextPiece) : rest
 
 -- | Check if piece data has correct hash.
 checkPieces :: PieceMgr -> Word32 -> B.ByteString -> IO Bool
 checkPieces (PieceMgr pSize totalSize pieces pData) pIdx pHash = do
     (arr, _) <- readMVar pData
-    let isLastPiece = pIdx == pieces - 1
-        start :: Word64
-        start = fromIntegral $ pIdx * pSize
-    bytes <- slice arr start $
-               if isLastPiece
-                 then start + (if totalSize `mod` fromIntegral pSize == 0
-                                 then fromIntegral pSize
-                                 else totalSize `mod` fromIntegral pSize)
-                 else start + fromIntegral pSize
+    let
+      isLastPiece = pIdx == pieces - 1
+      start :: Word64
+      start = fromIntegral pIdx * fromIntegral pSize
+      end :: Word64
+      end = if isLastPiece
+              then start + (if totalSize `mod` fromIntegral pSize == 0
+                              then fromIntegral pSize
+                              else totalSize `mod` fromIntegral pSize)
+              else start + fromIntegral pSize
+    bytes <- V.toList <$> V.freeze (MV.slice (fromIntegral start) (fromIntegral end) arr)
     return $ word160ToBS (hash bytes) == pHash
 
 generateFiles :: PieceMgr -> Info -> IO [(FilePath, B.ByteString)]
 generateFiles (PieceMgr _ _ _ pData) (Info name _ _ _ files) = do
-    let fs = map (\(p, s) -> (BC.unpack name </> p, s)) $ collectFiles files
-    bytes <- readMVar pData >>= A.getElems . fst
-    return $ zip (map fst fs) $ map B.pack $ splitBytes bytes (map snd fs)
+    let (names, sizes) = unzip $ map (\(p, s) -> (BC.unpack name </> p, s)) $ collectFiles files
+    bytes <- fst <$> readMVar pData
+    unless (fromIntegral (sum sizes) == MV.length bytes) . error . concat $
+      [ "sum sizes /= length bytes ("
+      , show (sum sizes), " /= ", show (MV.length bytes), ")" ]
+    bs <- splitBytes bytes sizes
+    return $ zip names $ map B.pack bs
   where
-    splitBytes :: [Word8] -> [Word64] -> [[Word8]]
-    splitBytes [] [] = []
-    splitBytes ws (i : is) =
-      let (h, t) = splitAt (fromIntegral i {- TODO: is this conversion safe? -}) ws
-          rest   = splitBytes t is
-      in (h : rest)
+    splitBytes :: MV.IOVector Word8 -> [Word64] -> IO [[Word8]]
+    splitBytes _  []       = return []
+    splitBytes ws (i : is) = do
+      let (h, t) = MV.splitAt (fromIntegral i {- TODO: is this conversion safe? -}) ws
+      rest <- splitBytes t is
+      hl <- V.toList <$> V.freeze h
+      return (hl : rest)
 
     collectFiles :: Either File [File] -> [(FilePath, Word64)]
     collectFiles (Left f)   = [collectF f]
@@ -119,16 +125,3 @@ generateFiles (PieceMgr _ _ _ pData) (Info name _ _ _ files) = do
 
     mkPath :: [B.ByteString] -> String
     mkPath bs = foldl' (</>) "" $ map BC.unpack bs
-
--- * Utils
-
--- FIXME: This may be too slow because of redundant arr -> list conversion.
--- FIXME: Maybe move to utils.
--- FIXME: Write tests before improving the performance.
-slice :: A.IOUArray Word64 Word8 -> Word64 -> Word64 -> IO [Word8]
-slice arr start end =
-    take (fromIntegral $ end - start) . drop (fromIntegral start) <$> A.getElems arr
-
-word160ToBS :: Word160 -> B.ByteString
-word160ToBS (Word160 w1 w2 w3 w4 w5) = LB.toStrict . BB.toLazyByteString . mconcat $
-    map BB.word32BE [w1, w2, w3, w4, w5]
