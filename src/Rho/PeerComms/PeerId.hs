ac0b7646c7673cc341987d9321c4db5b36bcb4a0
Ömer Sinan Ağacan <omeragacan@gmail.com>
2014-12-29 12:38:56 +0200

    fix peer id generation using rho-torrent signature

--- src/Rho/PeerComms/PeerId.hs
+++ src/Rho/PeerComms/PeerId.hs
@@ -1,17 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Rho.PeerComms.PeerId where
 
-import           Control.Applicative
 import           Control.Monad
-import qualified Data.ByteString         as B
-import qualified Data.ByteString.Builder as BB
-import qualified Data.ByteString.Lazy    as LB
+import qualified Data.ByteString as B
 import           Data.Monoid
-import           System.Random           (randomIO)
+import           System.Random   (randomIO)
 
 -- | 20-byte peer_id
 newtype PeerId = PeerId { unwrapPeerId :: B.ByteString } deriving (Show, Ord, Eq)
 
 -- | Generate 20-byte peer id.
 generatePeerId :: IO PeerId
-generatePeerId =
-    PeerId . LB.toStrict . BB.toLazyByteString . mconcat . map BB.word8 <$> replicateM 20 randomIO
+generatePeerId = (PeerId . ("-RH0100-" <>) . B.pack) `fmap` replicateM 12 randomIO
