a0bc98b9a6693a77eb635887f72f504509b72f58
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-10-05 17:18:08 -0400

    change Show PeerId for better debug outputs

--- src/Rho/PeerComms/PeerId.hs
+++ src/Rho/PeerComms/PeerId.hs
@@ -8,8 +8,11 @@ import           Data.Monoid
 import           System.Random   (randomIO)
 
 -- | 20-byte peer_id
-newtype PeerId = PeerId { unwrapPeerId :: B.ByteString } deriving (Show, Ord, Eq)
+newtype PeerId = PeerId { unwrapPeerId :: B.ByteString } deriving (Ord, Eq)
+
+instance Show PeerId where
+    show (PeerId bs) = "<PeerId " ++ show (B.dropWhile (== 48) bs) ++ ">"
 
 -- | Generate 20-byte peer id.
 generatePeerId :: IO PeerId
-generatePeerId = (PeerId . ("-RH0100-" <>) . B.pack) `fmap` replicateM 12 randomIO
+generatePeerId = (PeerId . ("-RH0100-" <>) . B.pack) <$> replicateM 12 randomIO
